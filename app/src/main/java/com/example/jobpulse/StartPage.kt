f797a79ca2871322da2d878d2cac7ece43186698
Krutik19 <krutikpatel822@gmail.com>
2024-03-14 17:58:42 -0400

    Login, Redister and details page

---
+++ app/src/main/java/com/example/jobpulse/StartPage.kt
@@ -0,0 +1,11 @@
+package com.example.jobpulse
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StartPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start_page2)
+    }
+}
